d785cfd004193f7bd974b21f1d22307c852f980e
NamidM <namid.marxen@gmail.com>
2021-04-21 13:04:49 +0200

    Added functions

--- index.php
+++ index.php
@@ -13,29 +13,6 @@ $userfollowers = $row->ergebnis;
 $sql = "SELECT COUNT(*) AS ergebnis FROM follows WHERE userID=".$_SESSION['userID'];
 $row = mysqli_fetch_object($db->query($sql));
 $userfollowing = $row->ergebnis;
-
-
-function showPostTime($time){
-    $time = strtotime($time);
-    $now = strtotime(date("Y-m-d H:i:s"));
-    $diff = $now - $time;
-    if($diff - 60 < 0) {
-        // Show seconds
-        return $diff." sek";
-    } elseif ($diff - 60*60 < 0) {
-        // Show minutes
-        return round($diff/60)." min";
-    } elseif ($diff - 60*60*24 < 0) {
-        // Show hours
-        return round($diff/60/60)." std";
-    } elseif (strftime("%Y", $time) == strftime("%Y", $now)) {
-        // Show date
-        return strftime("%d %h", $time);
-    } else {
-        // Show date and year
-        return strftime("%d %h %y", $time);
-    }
-}
 ?>
 
 <br>
@@ -108,47 +85,7 @@ function showPostTime($time){
                             ');
                             $query = htmlspecialchars("AND post.content LIKE '% ".$_GET['query']." %'");
                         }
-                        $sql = "SELECT post.*, user.username, user.verified FROM post, user WHERE user.id=post.userID ".$query." ORDER BY post.postDate DESC";
-                        $res = $db->query($sql);
-                        while($row = mysqli_fetch_object($res)) {
-                            $sql = "SELECT ((SELECT COUNT(*) FROM feedback WHERE `like` = 1 AND postID = ".$row->id.") - (SELECT COUNT(*) FROM feedback WHERE `like` = 0 AND postID = ".$row->id.")) AS ergebnis";
-                            if($row2 = mysqli_fetch_object($db->query($sql))) {
-                                $likecount = $row2->ergebnis;
-                            }
-                            $sql = "SELECT COUNT(*) AS ergebnis FROM post WHERE referencedPostID = ".$row->id;
-                            if($row2 = mysqli_fetch_object($db->query($sql))) {
-                                $replycount = $row2->ergebnis;
-                            }
-                            $sql = "SELECT * FROM feedback WHERE postID = ".$row->id." AND userID = ".$_SESSION['userID'];
-                            $liked = NULL;
-                            if($row2 = mysqli_fetch_object($db->query($sql))) {
-                                $liked = $row2->like;
-                            }
-                            $changedContent = preg_replace('/(?<= |^)(#[a-zA-Z0-9]+)(?= |$)/', '<span class="hashtag" onclick="search(\'$1\')">$1</span>', $row->content);
-                            $changedContent = preg_replace('/(?<= |^)(@[a-z0-9_-]{3,16}+)(?= |$)/', '<span class="username" onclick="openUser(\'$1\')">$1</span>', $changedContent);
-                            $changedContent = preg_replace('/((http|https)\:\/\/[a-zA-Z0-9\-\.]+\.[a-zA-Z]{2,3}(\/\S*)?)/im', '<a class="content-link" href="$1">$1</a>', $changedContent);
-                            echo('
-                            <div class="card post" style="width: 18rem;">
-                                <a href="profile.php?user='.$row->username.'"><img src="assets/images/cat2.png" class="posted-profile-pic"/></a>
-                                <div class="card-body post-content">
-                                    <h5 class="card-title post-headline">
-                                        <a class="post-username"  href="profile.php?user='.$row->username.'">'.$row->username.'</a> 
-                                        '.($row->verified ? '<b class="material-icons verified-follow">verified</b>' : '').'
-                                        <span class="card-subtitle mb-2 text-muted post-date">· &nbsp;' .showPostTime($row->postDate).'</span>
-                                    </h5>
-                                    <p class="card-text">'.$changedContent.'</p>
-                                    <img src="assets/images/cat.jpg" class="post-media"/><br><br>
-                                    <span onclick="feedback(1, '.$_SESSION['userID'].', '.$row->id.')" id="like-btn'.$row->id.'" class="material-icons feedback text-primary '.($liked == "1" ? 'text-success' : '').'">thumb_up</span>
-                                    <span class="like-count text-primary" id="like-count'.$row->id.'">'.$likecount.'</span>
-                                    <span onclick="feedback(0, '.$_SESSION['userID'].', '.$row->id.')" id="dislike-btn'.$row->id.'" class="text-primary material-icons feedback '.($liked == "0" ? 'text-danger' : '').'">thumb_down</span>
-                                    <div class="reply">
-                                        <a href="post.php?refPost='.$row->id.'" class="material-icons text-success reply-icon">reply</a>
-                                        <span class="reply-count text-success">'.$replycount.'</span>
-                                    </div>
-                                </div>
-                            </div>
-                            ');
-                        }
+                        echo getUserPosts(-1, $db, $query);
                     ?>
                 </div>
             </div>
--- profile.php
+++ profile.php
@@ -6,6 +6,7 @@ if(isset($_GET['user'])){
 
     $sql = "SELECT * FROM user WHERE username='" . htmlspecialchars($_GET['user']) . "'";
     $res = $db->query($sql);
+    $counter = 0;
     while($row = mysqli_fetch_object($res)) {
 
         $sql = "SELECT COUNT(*) AS ergebnis FROM follows WHERE `following`=".$row->id;
@@ -13,22 +14,23 @@ if(isset($_GET['user'])){
         $userfollowers = $row2->ergebnis;
 
         $sql = "SELECT COUNT(*) AS ergebnis FROM follows WHERE userID=".$row->id;
-        $row3 = mysqli_fetch_object($db->query($sql));
-        $userfollowing = $row3->ergebnis;
+        $row2 = mysqli_fetch_object($db->query($sql));
+        $userfollowing = $row2->ergebnis;
 
-        $selfPage = false;
-        $isFollowing = true;
+        $sql = "SELECT COUNT(*) AS ergebnis FROM follows WHERE userID=".$_SESSION['userID']." AND `following` = ".$row->id;
+        $row2 = mysqli_fetch_object($db->query($sql));
+        $isFollowing = $row2->ergebnis > 0 ? true : false;
 
         echo('
         <div class="container main-content">
             <div>
-                <img class="banner" src="' . $row->banner . '">
+                <img class="banner" src="assets/images/cat2.png' . $row->banner . '">
             </div>
             <div class="profile">
-                <img class="avatar" src="' . $row->avatar . '">
+                <img class="avatar" src="assets/images/cat2.png' . $row->avatar . '">
 
                 <div class="profile-actions">
-                    '. ($selfPage  ? '<button>Profil bearbeiten</button>' : ($isFollowing ? '<button class="following">Folge ich</button>' : '<button>Folgen</button>')) . ' 
+                    '. ($_GET['user'] == $_SESSION['username'] ? '<button>Profil bearbeiten</button>' : ($isFollowing ? '<button class="following">Folge ich</button>' : '<button>Folgen</button>')) . ' 
                 </div>
                 <br><br>
                 <p class="profile-displayname"><b>' . $row->username . '</b>' . ($row->verified ? '<b class="material-icons verified-follow">verified</b>' : '') . '</p>
@@ -41,33 +43,38 @@ if(isset($_GET['user'])){
                 </div>
 
                 <div class="tab">
-                    <button class="tablinks active" onclick="openProfileTabs(event, \'tweets\')">Tweets</button>
-                    <button class="tablinks" onclick="openProfileTabs(event, \'tweets-answers\')">Tweets und Antworten</button>
+                    <button class="tablinks active" onclick="openProfileTabs(event, \'posts\')">Posts</button>
+                    <button class="tablinks" onclick="openProfileTabs(event, \'posts-answers\')">Posts und Antworten</button>
                     <button class="tablinks" onclick="openProfileTabs(event, \'media\')">Medien</button>
                     <button class="tablinks" onclick="openProfileTabs(event, \'likes\')">Gefällt mir</button>
                 </div>
               
-                <div id="tweets" class="tabcontent" style="display: block">
-                    <h3>Tweets</h3>
+                <div id="posts" class="tabcontent" style="display: block">
+                    '.getUserPosts($row->id, $db, "", true).'
                 </div>
                 
-                <div id="tweets-answers" class="tabcontent">
-                    <h3>Tweets und Antworten</h3>
+                <div id="posts-answers" class="tabcontent">
+                    '.getUserPosts($row->id, $db, "AND post.referencedPostID IS NOT NULL", true).'
                 </div>
                 
                 <div id="media" class="tabcontent">
-                    <h3>Medien</h3>
+                    '.getUserPosts($row->id, $db, "AND post.media IS NOT NULL", true).'
                 </div>
                 
                 <div id="likes" class="tabcontent">
-                    <h3>Gefällt mir</h3>
+                    '.getUserPosts($row->id, $db, "AND feedback.like = 1 AND feedback.userID = ".$_SESSION["userID"]."", true, "INNER JOIN feedback ON feedback.postID = post.id").'
                 </div>
             </div>
         </div>
         ');
+        $counter++;
+    }
+    if($counter == 0) {
+        echo("<div class='main-content'>ff</div>");
     }
 } else{
     echo('Undefined User');
 }
+
 include('src/php/footer.php');
 ?>
