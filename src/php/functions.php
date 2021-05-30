00dc6db401ced728523854191ada453c1c99e979
NamidM <namid.marxen@gmail.com>
2021-05-30 17:14:54 +0200

    Small fix for post

--- src/php/functions.php
+++ src/php/functions.php
@@ -45,7 +45,9 @@ function getPosts($cond, $db, $showReplies = false, $second = false, $getParent 
             $posts .= loadReplies($post->id, 1, $db);
         }
     }
-    $posts = $posts != "" ? $posts : "<h4 class='no-posts'>Keine interessanten neuen Posts...</h4>";
+    if(!$second) {
+        $posts = $posts != "" ? $posts : "<h4 class='no-posts'>Keine interessanten neuen Posts...</h4>";
+    }
     if($showReplies && !$second) {
         $others = getPosts("post.referencedPostID IS NULL "
         . ( count($postIDs) != 0 ? "AND post.id NOT IN (" . implode(",", $postIDs) . ")" : ""), $db, true, true);
